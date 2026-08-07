1586aede6565f2482504fbc829a345674495942b
erfansy1857-dev <erfansy1857@gmail.com>
2026-08-07 08:56:24 +0700

    Update admin.js

--- admin.js
+++ admin.js
@@ -193,7 +193,8 @@ function renderAdminLaporan() {
   document.getElementById('lapTahun').innerText = `Rp ${totalTahun.toLocaleString('id-ID')}`;
   document.getElementById('lapTotal').innerText = `Rp ${totalKeseluruhan.toLocaleString('id-ID')}`;
 }
-
+// Initial View
+renderAdminDashboard();
 // Helper Simpan & Update Data
 function saveAndRefresh() {
   localStorage.setItem('produk', JSON.stringify(produkList));
